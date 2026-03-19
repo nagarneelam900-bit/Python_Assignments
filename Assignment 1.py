07eaafcfbf466d1b2ed3842bc93a8be650d0a4bc
nagarneelam900-bit <nagarneelam900@gmail.com>
2026-03-20 00:46:22 +0530

    Assignment Files

---
+++ Assignment 1.py
@@ -0,0 +1,181 @@
+
+Assignment 1
+
+
+a= 5
+b = 10
+print(a < b < 20)
+
+output:- True
+
+
+#2. Predict the output:
+x = True
+y = False
+print(x + y * x)
+
+output:- 1
+
+
+#3. What does this expression evaluate to?
+print(4 ** 0 ** 2)
+
+output:- 1
+
+
+#4. Find the result of this bitwise operation:
+a = 12 
+b = 5 
+print(a ^ b)
+
+ Output:- 9
+
+ #5. Guess the output:
+print((3 and 0) or (0 and 3))
+
+output:- 0 or 3
+
+ True
+
+#6. What's the output of this tricky comparison?
+print(256 is 256)
+print(257 is 257)
+
+output:-
+True 
+True 
+
+#7. Evaluate this expression:
+a = 7
+print(~a + 1)
+
+output:-
+-7
+
+8. What will this print?
+
+a = True
+b = False
+print((a or b) and not (a and b))
+
+
+False and True
+ output:-True
+
+9. What's the output?
+
+print(10 > 5 == True)
+
+output:-
+True
+
+10. Evaluate this expression:
+
+print(2 + 3 * 4 ** 2 // 8 % 3)
+
+output:-
+2
+
+#But i did calculation mistake here
+
+
+11. What does this expression evaluate to?
+
+print(1 << 2 + 1)
+output:- No Idea
+
+
+12. Predict the output:
+
+a = 3
+b = 2
+a *= b + 1
+print(a)
+
+output:-
+9
+# I did a mistake here also.Note - assignment operator priority is less than addition
+
+
+13. Evaluate this chained comparison:
+
+print(3 < 5 > 2 == 2)
+
+output:-
+False
+
+#Correct Answer is True. I did a mistake here also.
+
+14. Guess the result:
+
+print(10 // 3 * 3 + 1 % 3)
+
+output:-
+10
+#Note: I did mistake in modulus operator calculation.
+
+15. What is the result of this?
+"""
+x = 10
+y = 20
+print(x & y | x ^ y)
+"""
+output:- 20 | 30
+
+
+16 8 4 2 1
+0  1 0 1 0
+1  0 1 0 0
+1  1 1  10
+
+10100
+11110
+11110
+
+Output:- 30
+
+
+16. Trick with boolean and bitwise:
+
+a = True
+b = False
+print(a & b or a ^ b)
+
+0 or 1
+ output:- 1
+
+
+17. Evaluate this:
+
+x = 8
+print(x >> 1 << 2)
+
+Output:-16
+
+
+18. What does this produce?
+
+print(5 + True * False + not False)
+
+output:-
+Error
+#Slight correction in Question
+print(5 + True * False + (not False)
+output:-
+6
+
+19. Operator confusion:
+
+print((not 0) * (False or 1))
+
+output- true * true
+true
+
+20. Mix of precedence and associativity:
+
+print(4 + 3 * 2 ** 2 // 2 - 1)
+
+output:-
+9
+
+
---
+++ Assignment 2.py
@@ -0,0 +1,118 @@
+#Assignment 2
+#1. Write a program to print "Hello Python" on the screen
+
+print("Hello Python")
+
+Output:-
+
+Hello Python
+
+2. Write a program to take your name as input and print "Hello <name>".
+
+
+name=input("Enter the name:")
+print(f"Hello <{name}>")
+
+=========================================================== RESTART: C:/Users/Lenovo/Desktop/Python/Solutions/Assignment 2.py ==========================================================
+Enter the name:Neelam
+Hello <Neelam>
+
+
+#3. Write a program to take one integer input from the user and print it
+
+no=int(input("Enter the number:"))
+print(f"The number is :{no}")
+
+=========================================================== RESTART: C:/Users/Lenovo/Desktop/Python/Solutions/Assignment 2.py ==========================================================
+Enter the number:20
+The number is :20
+
+#4. Write a program to take two integers as input and print their sum.
+
+no1= int(input("Enter number 1:"))
+no2= int(input("Enter number 2:"))
+sum=no1+no2
+print("The sum is:",sum)
+
+=========================================================== RESTART: C:/Users/Lenovo/Desktop/Python/Solutions/Assignment 2.py ==========================================================
+Enter number 1:20
+Enter number 2:56
+The sum is: 76
+
+
+
+#5. Write a program to take two integers as input and print their multiplication.
+
+a= int(input("Enter number 1:"))
+b= int(input("Enter number 2:"))
+multiply= a*b
+print("The multiplication value is:",multiply)
+
+=========================================================== RESTART: C:/Users/Lenovo/Desktop/Python/Solutions/Assignment 2.py ==========================================================
+Enter number 1:5
+Enter number 2:2
+The multiplication value is: 10
+
+
+
+#6. Write a program to take one float number as input and print it.
+num=float(input("Enter the number:"))
+print("The number is:",num)
+
+=========================================================== RESTART: C:/Users/Lenovo/Desktop/Python/Solutions/Assignment 2.py ==========================================================
+Enter the number:45.6
+The number is: 45.6
+
+
+
+#7. Write a program to take two float numbers as input and print their average.
+a=float(input("Enter the number A:"))
+b=float(input("Enter the number B:"))
+average= (a+b)/2
+print("The average of two numbers is:",average)
+
+=========================================================== RESTART: C:/Users/Lenovo/Desktop/Python/Solutions/Assignment 2.py ==========================================================
+Enter the number A:44
+Enter the number B:88
+The average of two numbers is: 66.0
+
+#8. Write a program to take length and width (float) from the user and calculate the area of a rectangle.
+length= float(input("Enter the length of Rectangle:"))
+width=float(input("Enter the width of Rectangle:"))
+area= length*width
+print("Area of rectangle is:",area)
+
+=========================================================== RESTART: C:/Users/Lenovo/Desktop/Python/Solutions/Assignment 2.py ==========================================================
+Enter the length of Rectangle:12.0
+Enter the width of Rectangle:6.8
+Area of rectangle is: 81.6
+
+
+#9. Write a program to take an integer input and convert it into float, then print it.
+num=float(int(input("Enter the number:")))
+print("The number is:",num)
+
+=========================================================== RESTART: C:/Users/Lenovo/Desktop/Python/Solutions/Assignment 2.py ==========================================================
+Enter the number:10
+The number is: 10.0
+
+
+#10. Write a program to take price of three items (float) and print the total price.
+
+product1= float(input("Enter the price of product1:"))
+product2= float(input("Enter the price of product2:"))
+product3= float(input("Enter the price of product3:"))
+Total=product1+product2+product3
+print("The total price of all products is:",Total)
+
+=========================================================== RESTART: C:/Users/Lenovo/Desktop/Python/Solutions/Assignment 2.py ==========================================================
+Enter the price of product1:89
+Enter the price of product2:90
+Enter the price of product3:100
+The total price of all products is: 279.0
+
+
+
+
+
+
